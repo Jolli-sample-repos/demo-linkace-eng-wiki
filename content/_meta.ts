a4a38438febcfe71331787a7ab53b09d4f69241b
Joe Hart <joe@jeverest.com>
2025-12-10 19:01:50 -0800

    Add Docusaurus documentation site

---
+++ content/_meta.ts
@@ -0,0 +1,9 @@
+export default {
+  'index': 'Home',
+  'eng-faq-troubleshooting': 'ENG - FAQ & Troubleshooting',
+  'eng-data-model': 'ENG - Data Model',
+  'eng-how-to-guides': 'ENG - How to Guides',
+  'eng-components-and-modules': 'ENG - Components and Modules',
+  'eng-architecture': 'ENG - Architecture',
+  'eng-overview': 'ENG - Overview'
+}
