d5bae02898d6f242f6a5418ca2d491dd146acf80
Joe Hart <joe@jeverest.com>
2025-12-10 19:08:22 -0800

    Update content/_meta.ts
    Updated via Jolli repository editor

--- content/_meta.ts
+++ content/_meta.ts
@@ -2,7 +2,7 @@ export default {
   'index': 'Home',
   'eng-overview': 'Overview',
   'eng-architecture': 'Architecture',
-  'eng-components-and-modules': 'Components and Modules',
+  'eng-components-and-modules': 'Components & Modules',
   'eng-data-model': 'Data Model',
   'eng-how-to-guides': 'How to Guides',
   'eng-faq-troubleshooting': 'FAQ & Troubleshooting'
